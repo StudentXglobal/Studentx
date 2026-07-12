3e2941d3504078287cfa02c6569bf6a71e116395
Yusuf Alhaji Garba <tsirma001@gmail.com>
2026-07-12 17:36:24 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,27 @@
-alert("LOGIN.JS LOADED");
+document.getElementById("mainBtn").addEventListener("click", loginUser);
+
+async function loginUser() {
+  const email = document.getElementById("emailInput").value.trim();
+  const password = document.getElementById("passInput").value;
+
+  if (!email || !password) {
+    showMsg("Please enter email and password");
+    return;
+  }
+
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
+    email: email,
+    password: password
+  });
+
+  if (error) {
+    showMsg("Invalid email or password");
+    return;
+  }
+
+  showMsg("Login successful");
+
+  setTimeout(() => {
+    window.location.href = "home.html";
+  }, 1000);
+}
