24ea49c66ca549f8fac199b2806d91b2f1be9055
Yusuf Alhaji Garba <tsirma001@gmail.com>
2026-07-12 13:33:19 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL = "https://acragelglvdnqhvbjcgx.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_O2kgvCZ8CxPKghH7MJn7nw_R-l61YuH;
+const SUPABASE_ANON_KEY = "sb_publishable_O2kgvCZ8CxPKghH7MJn7nw_R-l61YuH";
 
-window.supabaseClient = window.supabase.createClient(
+const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
