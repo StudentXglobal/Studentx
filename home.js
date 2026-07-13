582e5fa8c3b511ae74573c7435866e9f436b1172
Yusuf Alhaji Garba <tsirma001@gmail.com>
2026-07-13 02:20:41 +0100

    Update home.js

--- home.js
+++ home.js
@@ -1,33 +1,41 @@
 document.addEventListener("DOMContentLoaded", async () => {
+
     const {
         data: { user },
         error
     } = await supabaseClient.auth.getUser();
 
     if (error || !user) {
+        alert("User not logged in");
         window.location.href = "login.html";
         return;
     }
 
+    alert("User ID:\n" + user.id);
+
     const { data: profile, error: profileError } = await supabaseClient
-  .from("profiles")
-  .select("*")
-  .eq("id", user.id)
-  .maybeSingle();
+        .from("profiles")
+        .select("*")
+        .eq("id", user.id)
+        .maybeSingle();
 
     if (profileError) {
         alert(profileError.message);
         return;
     }
 
-    document.getElementById("fullName").textContent =
-        profile.full_name || "StudentX User";
+    if (!profile) {
+        alert("Profile not found");
+        return;
+    }
 
-    document.getElementById("username").textContent =
-        "@" + (profile.username || "student");
+    alert("Profile found!");
+
+    document.getElementById("fullName").textContent = profile.full_name;
+    document.getElementById("username").textContent = "@" + profile.username;
 
     if (profile.avatar_url) {
-        document.getElementById("profileAvatar").src =
-            profile.avatar_url;
+        document.getElementById("profileAvatar").src = profile.avatar_url;
     }
+
 });
